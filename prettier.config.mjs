74d205355adddf237d6a227e31d8526bfb7184ea
AlonsoGaray <alonsogarayp@gmail.com>
2024-01-16 18:34:51 -0500

    Update files with prettier config

---
+++ prettier.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import("prettier").Config} */
+export default {
+  plugins: ['prettier-plugin-astro'],
+  tabWidth: 2,
+  trailingComma: 'none',
+  semi: false,
+  singleQuote: true,
+  jsxSingleQuote: true,
+  printWidth: 120
+}
