5b223f5ddfb12deeb5560014deed0c7290cb8a7e
AlonsoGaray <alonsogarayp@gmail.com>
2024-01-16 20:10:42 -0500

    Update prettier.config.mjs and About.astro, delete Card.astro and newFile.tsx

--- prettier.config.mjs
+++ prettier.config.mjs
@@ -6,5 +6,5 @@ export default {
   semi: false,
   singleQuote: true,
   jsxSingleQuote: true,
-  printWidth: 120
+  printWidth: 80
 }
--- src/components/newFile.tsx
+++
@@ -1,6 +0,0 @@
-/** @jsxImportSource astro */
-<Fragment>
-<a {...Astro.props} class="flex bg-slate-800 p-2 rounded-full">
-<slot />
-</a>
-</Fragment>;
